186edeb536e0093a8a1e657050ca34745da69ce5
amicoded <amicoded@gmail.com>
2016-04-01 11:21:36 +0530

    Indentation fixes

--- src/codes/LongestIncreasingSubsequence.java
+++ src/codes/LongestIncreasingSubsequence.java
@@ -1,25 +1,28 @@
 package codes;
 
 public class LongestIncreasingSubsequence {
-	
-	public static int longestIncresingSubsequence(int[] nums) {
-		if(nums == null || nums.length == 0) return 0;
-		int[] subsequenceLengthArr = new int[nums.length];
-		
-		for(int i = 0; i < nums.length; i++) {
-			subsequenceLengthArr[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(nums[j] < nums[i]) {
-					subsequenceLengthArr[i] = Math.max(subsequenceLengthArr[j]+1, subsequenceLengthArr[i]);	
-				}
-			}
-		}
-		return subsequenceLengthArr[nums.length-1];
-	}
-	
-	public static void main(String[] args) {
-		int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60, 55, 65, 64, 66};
-		System.out.println(longestIncresingSubsequence(arr));
-	}
+
+    public static int longestIncresingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int[] subsequenceLengthArr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            subsequenceLengthArr[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    subsequenceLengthArr[i] = Math.max(
+                            subsequenceLengthArr[j] + 1,
+                            subsequenceLengthArr[i]);
+                }
+            }
+        }
+        return subsequenceLengthArr[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60, 55, 65, 64, 66 };
+        System.out.println(longestIncresingSubsequence(arr));
+    }
 
 }
