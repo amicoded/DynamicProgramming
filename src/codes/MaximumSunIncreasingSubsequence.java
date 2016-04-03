c724c7927687dbb0d2ac029c7eadf6300d163a05
amicoded <amicoded@gmail.com>
2016-04-04 00:52:29 +0530

    MaximumSunIncreasingSubsequence

---
+++ src/codes/MaximumSunIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+package codes;
+
+import java.util.Arrays;
+
+public class MaximumSunIncreasingSubsequence {
+
+    public static int maximumSumIncresingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int[] sumArr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sumArr[i] = nums[i];
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    sumArr[i] = Math.max(
+                            sumArr[j] + nums[i],
+                            sumArr[i]);
+                }
+            }
+        }
+        return Arrays.stream(sumArr).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 3, 4, 21, 5 , 18, 12};
+        System.out.println(maximumSumIncresingSubsequence(arr));
+    }
+
+}
